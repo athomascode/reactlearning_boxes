046e1e3a97cb06b9f7d5b9d8d2386808bcd5f1f7
athomascode <aishwarya.thomas35@gmail.com>
2023-08-04 09:15:20 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ export default function App(props) {
       onClick={() => changeState(element.id)}/>
     ))
 
+    const [messages, setMessages] = React.useState(["a","b"])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -32,6 +34,9 @@ export default function App(props) {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {!messages.length ? <h1>zero unread</h1> : <h1>{messages.length} unread</h1>}
+        {!messages.length && <h3>You're all caught up!</h3>}
+        {messages.length > 0 && <h3>You have {messages.length} unread {messages.length === 1 ? "message" : "messages"}</h3>}
         <a
           className="App-link"
           href="https://reactjs.org"
